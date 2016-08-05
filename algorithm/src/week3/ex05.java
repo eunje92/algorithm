a973c29bd734159a5a5ac6bc918245a4546f20fb
eunje92 <dmswp92@hanmail.net>
2016-08-05 22:48:51 +0900

    알고리즘 3주차

--- algorithm/src/week3/ex05.java
+++ algorithm/src/week3/ex05.java
@@ -1,5 +1,39 @@
 package week3;
 
-public class ex05 {
+import java.util.Scanner;
 
+public class ex05 {
+	// 최대 증가 부분 수열(LIS)
+	Scanner sc=new Scanner(System.in);
+	int n;
+	int kids[]=new int[200];
+	
+	void main(){
+		n=sc.nextInt();
+		kids=new int[n];
+	
+		for(int i=0; i<n; i++){
+			kids[i]=sc.nextInt();
+		}
+		
+		System.out.println(n-lis(0));
+	}
+	
+	int lis(int start){
+		int ret=kids[start];
+		
+		if(ret==-1) return ret;
+		
+		ret=1;
+		for(int next=start+1; next<n; next++){
+			if(kids[start]<kids[next]){
+				ret = Math.max(ret, lis(next)+1);
+			}
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		new ex05().main();
+	}
 }
--- algorithm/src/week3/ex07.java
+++ algorithm/src/week3/ex07.java
@@ -1,5 +1,53 @@
 package week3;
 
-public class ex07 {
+import java.util.Scanner;
+import java.util.Vector;
 
+public class ex07 {
+	Scanner sc=new Scanner(System.in);
+	int n[]=new int[9];
+	boolean flag=false;
+	
+	void main(){
+		for(int i=0; i<9; i++){
+			n[i]=sc.nextInt();
+		}
+		boolean chk[]=new boolean[9];
+		Vector<Integer> list=new Vector<>();
+		
+		run(0, chk, list);
+	}
+	
+	void run(int num, boolean chk[], Vector<Integer> list){	
+		int sum=0;
+		if(flag){
+			return;
+		}
+	
+		if(num==7){
+			for(int i=0; i<list.size(); i++){
+				sum+=list.get(i);
+			}
+			if(sum==100){
+				for(int i=0; i<list.size(); i++){
+					System.out.println(list.get(i));
+				}
+				flag=true;
+				return;
+			}
+		}
+
+		for(int i=0; i<n.length; i++){
+			if(chk[i]) continue;
+			list.add(n[i]);
+			chk[i]=true;
+			run(num+1, chk, list);
+			list.remove(list.size()-1);
+			chk[i]=false;
+		}
+		
+	}
+	public static void main(String[] args) {
+		new ex07().main();
+	}
 }
